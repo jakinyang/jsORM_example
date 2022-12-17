47846c1a518442c251b2f316d0d51113bbfb065a
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-12-17 10:27:03 -0800

    Add Sequelize config and authentication

--- db/connection.js
+++ db/connection.js
@@ -3,15 +3,30 @@
 // to extract the "Pool" class
 const { Pool } = require('pg');
 
+// Require seequelize
+const { Sequelize } = require('sequelize');
 
-// Create a new pool object from the pool class
-// Pass a config objects with the details of our database
-const poolDataBase = new Pool({
+// Here we are creating a new sequelize object instance from the Sequelize class
+// We are handing it an options object with all of the configurations we need to access our particular database
+const sequelize = new Sequelize({
   host: 'localhost',
-  user: 'cheesepartyhost',
+  dialect: 'postgres',
+  username: 'cheesepartyhost',
   password: 'cheeseforever',
   database: 'cheeseparty',
-  port: '5432'
-});
+  port: '5432',
+})
 
-module.exports = { poolDataBase };
+
+// All sequelize methods are async and return promises
+// So methods like .then, .catch can be used out of the box
+// Testing connection
+sequelize.authenticate()
+  .then((res) => {
+    console.log("Sequelize connection to cheeseparty database success!");
+    sequelize.close();
+  })
+  .catch((error) => {
+    console.error("Sequelize unable to connect to cheeseparty database: ", error);
+    sequelize.close();
+  });
